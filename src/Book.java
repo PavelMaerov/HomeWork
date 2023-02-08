129f4f2f2da99be4d581eead606e91c63db30fbb
Pavel Maerov <pavel.a.maerov@gmail.com>
2023-02-08 17:23:51 +0100

    Перенес Author в отдельный файл

--- src/Book.java
+++ src/Book.java
@@ -29,21 +29,5 @@ public class Book {
     }
 }
 
-//2.Создайте класс Author, который содержит в себе данные об имени и фамилии автора.
-class Author {
-    private String firstName;
-    private String secondName;
-
-    public Author(String firstName, String secondName) {
-        this.firstName = firstName;
-        this.secondName = secondName;
-    }
-    public String getFirstName() {
-        return (firstName);
-    }
-    public String getSecondName() {
-        return (secondName);
-    }
-}
 
 
